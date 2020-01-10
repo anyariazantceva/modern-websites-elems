8a9a467ddee39399c7e53752124432aeffea83f5
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-01-10 22:18:58 +0200

    Made some changes in styles

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-//
+// Toggling sidebar
 function showSidebar () {
     let sidebar = document.querySelector('.sidebar');
     const menu = document.querySelector('.menu__icon');
     menu.addEventListener('click', () => {
-       if (sidebar.style.opacity === '0') {
-           sidebar.style.opacity = '1';
-           sidebar.style.left = '0px';
+       if (sidebar.style.opacity == 0) {
+           sidebar.style.opacity = 1;
+           sidebar.style.left = 0;
 
        } else {
-           sidebar.style.opacity = '0';
+           sidebar.style.opacity = 0;
            sidebar.style.left = '-1000px';
        }
     });
 }
 
 showSidebar();
+
+//
